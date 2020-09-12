5e015ce3646b45226001ae90b5b5466baca8462a
Manuel Martinez <33674827+alaskanbear48@users.noreply.github.com>
2020-09-12 10:51:44 -0500

    alpha deadline one

---
+++ Assets/tempTeller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using TMPro;
+
+public class tempTeller : MonoBehaviour
+{
+    
+    public GameObject tempTextObject;
+       string url = "http://api.openweathermap.org/data/2.5/weather?lat=41.88&lon=-87.6&APPID=acc4bbc98bd69f9619ec12b379277df0&units=imperial";
+
+   
+    void Start()
+    {
+
+    // wait a couple seconds to start and then refresh every 900 seconds
+
+       InvokeRepeating("GetDataFromWeb", 2f, 900f);
+   }
+
+   void GetDataFromWeb()
+   {
+
+       StartCoroutine(GetRequest(url));
+   }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                // print out the weather data to make sure it makes sense
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+
+                
+                var weather = JsonConvert.DeserializeObject<WeatherResponse>(webRequest.downloadHandler.text);
+
+                tempTextObject.GetComponent<TextMeshPro>().text = weather.main.temp.ToString();
+
+                
+
+            }
+        }
+    }
+}
